1d7e103fc636798ec3bfd8ebc980264ecea3217f
Luis Vallejo <luis.vallejo@gmail.com>
2013-08-20 19:52:52 +0800

    Intersect Line Method added for "AI"

--- main.lua
+++ main.lua
@@ -15,16 +15,21 @@ function love.load()
 	--ball
 	ballX = screenWidth / 2
 	ballY = screenHeight / 2
-	ballVelocityX = 300
-	ballVelocityY = -30
+	print(ballX .. "," .. ballY)
+	ballVelocityX = 200
+	ballVelocityY = -150
 
 	-- Paddle 1
 	paddle1X = 0
 	paddle1Y = screenHeight / 2
+	paddle1Velocity = 600
 
 	--Paddle 2
 	paddle2X = screenWidth - paddle2:getWidth()
 	paddle2Y = screenHeight / 2
+	paddle2Velocity = 1
+
+	ballBounce = true
 	times = 0
 end
 
@@ -51,19 +56,32 @@ function love.update(dt)
         paddle1Y = paddle1Y + dt * 600  -- 600 px/s sounds ok
     end
 
-
+    ballY = ballY + dt * ballVelocityY
+  	ballX = ballX + dt * ballVelocityX
+  	--print("Ball:(" .. ballX .. "," .. ballY ..")")
     -- logic for paddle 2 ( CPU )
-    if(ballVelocityX > 0) then
+    
+    if(ballBounce) then
+    	if(ballVelocityX > 0) then
+    		ty = line_intersect(ballX,ballY,ballVelocityX,ballVelocityY)
+    		print(ty)
+    	-- Going down
+    		if(ty > paddle2Y) then
+    			paddle2Y = ty - dt * paddle2Velocity
+    			print(paddle2Y)
+    			ballBounce = false
+    		else
+    			paddle2Y = ty + dt * paddle2Velocity
+    			ballBounce = false
+    		end
 
-    	if(paddle2Y + paddle2:getHeight() / 2 > ballY) then
-    		paddle2Y = paddle2Y - dt * 60
-    	end
-    	if(paddle2Y + paddle2:getHeight() / 2 < ballY ) then
-    		paddle2Y = paddle2Y + dt * 60
     	end
     end
 
+    
+    
 
+    
 	-- If ballX moves faster than y at the beggining then it will never
 	-- bounce of uppper and lower walls first.
 	-- dt * speedX = means it will move that amount of pixels per second
@@ -74,6 +92,7 @@ function love.update(dt)
 		if(ballVelocityX < 0) then
 			-- print(ballVelocityX)
 			ballVelocityX = ballVelocityX * -1
+			ballBounce = true
 		end
 	end
 	-- Hit Test for Paddle 2
@@ -81,14 +100,33 @@ function love.update(dt)
 		and ((paddle2Y + paddle2:getHeight() >= ballY) and (paddle2Y <= ballY + ball:getHeight())) then
 		if(ballVelocityX > 0) then
 			ballVelocityX = ballVelocityX * -1
+			ballBounce = true
 		end
 	end
 
-  	ballX = ballX + dt * ballVelocityX
-  	ballY = ballY + dt * ballVelocityY
+	if(ballY < 0) then
+		ballVelocityY = ballVelocityY * -1
+		ballBounce = true
+	end
+	if(ballY > screenHeight - ball:getHeight()) then
+		ballVelocityY = ballVelocityY * -1
+		ballBounce = true
+	end
+	
+  	
   	
 end
 
+function line_intersect(bx,by,vx,vy)
+	-- tx = target of X
+	 tx = screenWidth - 16 --- 16 being the width of the paddle
+	 slope = (tx - bx) / vx
+	 ty = by + slope * vy
+	 return ty
+end
+
+
+-- Unused 
 function detect_collision(p,px,py,b,bx,by)
 	-- body
 	--Hit test for Paddle 1
